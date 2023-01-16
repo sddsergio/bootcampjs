e1c5e843d7aa8fd52c983745340865a05ad9d88e
dsergiod <destefanosergio@hotmail.com>
2023-01-16 15:23:49 -0300

    first commit

---
+++ part0/script.js
@@ -0,0 +1,25 @@
+// primerConsoleLog = console.log('Hola Mundo');
+// primerConsoleLog = "hola"
+// console.log(primerConsoleLog)
+
+const myObj = {
+    nombre: 'Sergio',
+    apellido: 'Destefano',
+    edad: '32',
+    isDeveloper: true,
+    links: ['www.youtube.com.ar','www.google.com.ar']
+}
+
+
+console.log('Mi apellido es: ' + myObj.apellido); 
+console.log(myObj.edad);
+console.log(myObj.links[0]);
+
+// const sumar = (a, b) => {
+//     console.log(a);
+//     console.log(b);
+//     return a + b
+// }
+// const op1 = 5;
+// const op2 = 6;
+// console.log(sumar(op1, op2));
