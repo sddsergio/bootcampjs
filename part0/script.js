0ee7e15c7d22608cc8847faad7385af5cc4a5c59
dsergiod <destefanosergio@hotmail.com>
2023-01-16 17:18:11 -0300

    modificando objetos

--- part0/script.js
+++ part0/script.js
@@ -1,25 +1,22 @@
-// primerConsoleLog = console.log('Hola Mundo');
-// primerConsoleLog = "hola"
-// console.log(primerConsoleLog)
+const firstName = "Sergio";
+const firstNameUppercase = firstName.toUpperCase();
+const list = [];
+const anotherList = list.concat(100);
 
 const myObj = {
     nombre: 'Sergio',
     apellido: 'Destefano',
+    youtube: '@sdes0211',
     edad: '32',
     isDeveloper: true,
-    links: ['www.youtube.com.ar','www.google.com.ar']
+    links: ['www.youtube.com.ar','www.google.com.ar', '']
 }
-
+const field = 'youtube';
 
 console.log('Mi apellido es: ' + myObj.apellido); 
 console.log(myObj.edad);
-console.log(myObj.links[0]);
+console.log('Página web: ' + myObj.links[1]);
+console.log(myObj[field]);
+console.log(firstNameUppercase);
+console.log(anotherList[0]);
 
-// const sumar = (a, b) => {
-//     console.log(a);
-//     console.log(b);
-//     return a + b
-// }
-// const op1 = 5;
-// const op2 = 6;
-// console.log(sumar(op1, op2));
